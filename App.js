ab79204ecda3034a7dd453884c68f42add6f8eef
stradler <eamalakhov@gmail.com>
2018-10-15 11:16:58 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FlatList, ActivityIndicator, Text, View  } from 'react-native';
+
+export default class App extends React.Component {
+  render(){
+    return(
+      <div>Hello!</div>
+    );
+  }
+}
