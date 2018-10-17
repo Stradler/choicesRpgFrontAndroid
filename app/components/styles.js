c3d3d93ac88d42336e28d1760c95ed88b24469b8
Stradler <eamalakhov@gmail.com>
2018-10-17 14:08:21 +0300

    calculate center with dimensions help

--- app/components/styles.js
+++ app/components/styles.js
@@ -1,8 +1,11 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 export const styles = StyleSheet.create({
   gamewindow: {
+    display: "flex",
+    flexDirection: "column",
     alignItems: "center",
     justifyContent: "flex-end",
-    marginTop: 150
+    width: Dimensions.get("window").width,
+    marginTop: Dimensions.get("window").height / 3
   }
 });
