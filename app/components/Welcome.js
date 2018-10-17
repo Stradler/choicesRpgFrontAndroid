def75cea27265f9c26ac04940bbb661c5bd20bdc
Stradler <eamalakhov@gmail.com>
2018-10-17 14:09:17 +0300

    add styles object to welcom component

--- app/components/Welcome.js
+++ app/components/Welcome.js
@@ -1,36 +1,32 @@
 import React from "react";
 import { version } from "../../app.json";
-import { View } from "react-native";
-import { Button, Text } from "native-base";
+import { View, Dimensions } from "react-native";
+import { Button, Text, Content } from "native-base";
 import { Actions } from "react-native-router-flux";
+import { styles } from "./styles";
 
 const Welcome = () => {
   return (
-    <View
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        marginTop: 150
-      }}
-    >
-      <Text
-        style={{
-          fontSize: 40
-        }}
-      >
-        Choices Rpg
-      </Text>
-      <Text>
-        v.
-        {version}
-      </Text>
-      <View>
-        <Button title="Play!" onPress={() => Actions.game()}>
-          <Text>Играть!</Text>
-        </Button>
+    <Content>
+      <View style={styles.gamewindow}>
+        <Text
+          style={{
+            fontSize: 40
+          }}
+        >
+          Choices Rpg
+        </Text>
+        <Text>
+          v.
+          {version}
+        </Text>
+        <View>
+          <Button title="Play!" onPress={() => Actions.game()}>
+            <Text>Играть!</Text>
+          </Button>
+        </View>
       </View>
-    </View>
+    </Content>
   );
 };
 export default Welcome;
