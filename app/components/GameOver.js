2437517765ff01ac7b6e3587ac9da0c94703ace0
Stradler <eamalakhov@gmail.com>
2018-10-18 14:18:00 +0300

    add in animation to gameover component

--- app/components/GameOver.js
+++ app/components/GameOver.js
@@ -2,19 +2,27 @@ import React from "react";
 import { View } from "react-native";
 import { Text, Button } from "native-base";
 import { styles } from "./styles";
-const GameOver = props => {
-  return (
-    <View style={styles.gamewindow}>
-      <Text>{props.message}</Text>
-      <View>
-        <Button
-          title="Click here to start a new game!"
-          onPress={props.dispatchReset}
-        >
-          <Text>Начать заново!</Text>
-        </Button>
-      </View>
-    </View>
-  );
-};
+import * as Animatable from "react-native-animatable";
+class GameOver extends React.Component {
+  componentDidMount() {
+    this.view.lightSpeedIn(500);
+  }
+  render() {
+    const props = this.props;
+    return (
+      <Animatable.View ref={ref => (this.view = ref)} style={styles.gamewindow}>
+        <Text>{props.message}</Text>
+        <View>
+          <Button
+            title="Click here to start a new game!"
+            onPress={props.dispatchReset}
+          >
+            <Text>Начать заново!</Text>
+          </Button>
+        </View>
+      </Animatable.View>
+    );
+  }
+}
+GameOver = Animatable.createAnimatableComponent(GameOver);
 export default GameOver;
