fc85ebef462a2b1f516972378ac36ff8931d2648
Stradler <eamalakhov@gmail.com>
2018-10-18 17:32:38 +0300

    add typewriter to gameover screen

--- app/components/GameOver.js
+++ app/components/GameOver.js
@@ -3,15 +3,22 @@ import { View } from "react-native";
 import { Text, Button } from "native-base";
 import { styles } from "./styles";
 import * as Animatable from "react-native-animatable";
+import TypeWriter from "react-native-typewriter";
 class GameOver extends React.Component {
   componentDidMount() {
-    this.view.lightSpeedIn(500);
+    this.view.lightSpeedIn(100);
   }
   render() {
     const props = this.props;
     return (
       <Animatable.View ref={ref => (this.view = ref)} style={styles.gamewindow}>
-        <Text>{props.message}</Text>
+        <TypeWriter
+          style={{ textAlign: "center", fontSize: 30, marginBottom: 10 }}
+          typing={1}
+          maxDelay={50}
+        >
+          {props.message}
+        </TypeWriter>
         <View>
           <Button
             title="Click here to start a new game!"
