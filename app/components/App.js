0d2a30e0a14141d2813cf5c9aec32a7e7cf56244
Stradler <eamalakhov@gmail.com>
2018-10-19 18:11:38 +0300

    made login and signup dispatchers

--- app/components/App.js
+++ app/components/App.js
@@ -6,6 +6,7 @@ import { connect } from "react-redux";
 import Icon from "react-native-vector-icons/FontAwesome5";
 import getTheme from "./native-base-theme/components";
 import commonColor from "./native-base-theme/variables/commonColor";
+import CustomForm from "./Form";
 import {
   Container,
   Footer,
@@ -15,7 +16,8 @@ import {
   Title,
   Button,
   Content,
-  StyleProvider
+  StyleProvider,
+  Spinner
 } from "native-base";
 import { FlatList } from "react-native";
 const ReduxRouter = connect()(Router);
@@ -47,14 +49,53 @@ class DrawerList extends React.Component {
     );
   }
 }
+
+const Login = props => {
+  if (props.token && props.token != "lel") {
+    return Actions.welcome();
+  }
+  if (props.token === "lel")
+    return (
+      <CustomForm
+        errorMessage={true}
+        formAction={props.loginUser}
+        buttonText="Sign In"
+      />
+    );
+
+  return <CustomForm formAction={props.loginUser} buttonText="Sign In" />;
+};
+
+const Signup = props => {
+  if (props.token && props.token != "lel") {
+    return Actions.welcome();
+  }
+  if (props.token === "lel") {
+    return (
+      <CustomForm
+        errorMessage={true}
+        formAction={props.signupUser}
+        buttonText="Sign Up"
+      />
+    );
+  }
+  return <CustomForm formAction={props.signupUser} buttonText="Sign Up" />;
+};
 class App extends React.Component {
   render() {
-    const { events, error, fetching } = this.props;
+    console.log(this.props);
+    const { events, error, fetching, token } = this.props;
     if (!events || events.length === 0) {
       setTimeout(() => {
         this.props.onRequestEvents();
       }, 3000);
-      return <Text>Loading...</Text>;
+      return (
+        <Container>
+          <Content>
+            <Spinner />
+          </Content>
+        </Container>
+      );
     }
     return (
       <StyleProvider style={getTheme(commonColor)}>
@@ -67,7 +108,25 @@ class App extends React.Component {
               drawerIcon={<Icon name="eye" size={40} color="steelblue" />}
             >
               <Scene key="root" hideNavBar={false}>
-                <Scene key="welcome" component={Welcome} hideNavBar={false} />
+                <Scene
+                  key="welcome"
+                  component={() => <Welcome token={token} />}
+                  hideNavBar={false}
+                />
+                <Scene
+                  key="login"
+                  component={() => (
+                    <Login loginUser={this.props.loginUser} token={token} />
+                  )}
+                  hideNavBar={false}
+                />
+                <Scene
+                  key="signup"
+                  component={() => (
+                    <Signup signupUser={this.props.signupUser} token={token} />
+                  )}
+                  hideNavBar={false}
+                />
                 <Scene
                   key="game"
                   component={() => (
@@ -94,13 +153,19 @@ const mapStateToProps = state => {
   return {
     fetching: state.fetching,
     events: state.mainEvents,
-    error: state.error
+    error: state.error,
+    token: state.token,
+    userId: state.userId
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onRequestEvents: () => dispatch({ type: "API_CALL_REQUEST_MAIN" })
+    onRequestEvents: () => dispatch({ type: "API_CALL_REQUEST_MAIN" }),
+    loginUser: (login, password) =>
+      dispatch({ type: "API_CALL_LOGIN", payload: { login, password } }),
+    signupUser: (login, password) =>
+      dispatch({ type: "API_CALL_SIGNUP", payload: { login, password } })
   };
 };
 
