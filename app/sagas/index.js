225001a686e3b605cc4d9f3dfbcd6e3a567828de
Stradler <eamalakhov@gmail.com>
2018-10-19 18:09:21 +0300

    add sagas for login, signup, etc

--- app/sagas/index.js
+++ app/sagas/index.js
@@ -1,35 +1,106 @@
 import { takeEvery, call, put, all } from "redux-saga/effects";
-import axios from "axios";
-
+import * as API from "../config/api";
 // watcher saga: watches for actions dispatched to the store, starts worker saga
 export function* watcherSaga() {
   yield all([
     takeEvery("API_CALL_REQUEST_SURVIVAL", survivalSaga),
-    takeEvery("API_CALL_REQUEST_MAIN", mainSaga)
+    takeEvery("API_CALL_REQUEST_MAIN", mainSaga),
+    takeEvery("API_CALL_LOGIN", loginSaga),
+    takeEvery("API_CALL_SIGNUP", signupSaga),
+    takeEvery("API_CALL_GET_USER_INFO", getUserInfoSaga),
+    takeEvery("API_CALL_UPDATE_INFO", updateInfoSaga)
   ]);
 }
 
-// function that makes the api request and returns a Promise for response
-function fetchMainEvents() {
-  return axios({
-    method: "get",
-    url: "https://heroku-choices-rpg.herokuapp.com/api/main"
-  });
+function* updateInfoSaga(action) {
+  const { hp, age, money, token, currentEvent } = action.payload;
+  try {
+    const response = yield call(
+      API.updateInfo,
+      hp,
+      age,
+      money,
+      token,
+      currentEvent
+    );
+    const user = response.data;
+    if (user.status) {
+      yield put({ type: "API_CALL_UPDATE_INFO_SUCCESS", user });
+    } else {
+      yield put({ type: "API_CALL_UPDATE_INFO_FAILURE", kek: "lel" });
+    }
+
+    // dispatch a success action to the store with the new dog
+  } catch (error) {
+    // dispatch a failure action to the store with the error
+    yield put({ type: "API_CALL_UPDATE_INFO_FAILURE", kek: "lel" });
+  }
 }
 
-function fetchSurvivalEvents(age) {
-  return axios({
-    method: "get",
-    url: "https://heroku-choices-rpg.herokuapp.com/api/survival",
-    params: {
-      age
+function* getUserInfoSaga(action) {
+  try {
+    const response = yield call(API.getInfo, action.payload.token);
+    const user = response.data;
+    if (user.status) {
+      yield put({ type: "API_CALL_LOGIN_SUCCESS", ...user });
+    } else {
+      yield put({ type: "API_CALL_LOGIN_FAILURE", kek: "lel" });
     }
-  });
+
+    // dispatch a success action to the store with the new dog
+  } catch (error) {
+    // dispatch a failure action to the store with the error
+    yield put({ type: "API_CALL_LOGIN_FAILURE", kek: "lel" });
+  }
+}
+
+// function that makes the api request and returns a Promise for response
+function* loginSaga(action) {
+  try {
+    const response = yield call(
+      API.logIn,
+      action.payload.login,
+      action.payload.password
+    );
+    const user = response.data;
+    if (user.status) {
+      yield put({ type: "API_CALL_LOGIN_SUCCESS", ...user });
+    } else {
+      yield put({ type: "API_CALL_LOGIN_FAILURE", kek: "lel" });
+    }
+
+    // dispatch a success action to the store with the new dog
+  } catch (error) {
+    // dispatch a failure action to the store with the error
+    yield put({ type: "API_CALL_LOGIN_FAILURE", kek: "lel" });
+  }
+}
+
+function* signupSaga(action) {
+  try {
+    const response = yield call(
+      API.signUp,
+      action.payload.login,
+      action.payload.password
+    );
+    const user = response.data;
+    if (user.status) {
+      console.log(user);
+      yield put({ type: "API_CALL_SIGNUP_SUCCESS", ...user });
+    } else {
+      yield put({ type: "API_CALL_SIGNUP_FAILURE", kek: "lel" });
+    }
+
+    // dispatch a success action to the store with the new dog
+  } catch (error) {
+    // dispatch a failure action to the store with the error
+    yield put({ type: "API_CALL_SIGNUP_FAILURE", kek: "lel" });
+  }
 }
 
 function* survivalSaga(action) {
   try {
-    const response = yield call(fetchSurvivalEvents, action.payload.age);
+    const response = yield call(API.fetchSurvivalEvents, action.payload.age);
     const events = response.data;
 
     // dispatch a success action to the store with the new dog
@@ -42,7 +113,7 @@ function* survivalSaga(action) {
 
 function* mainSaga() {
   try {
-    const response = yield call(fetchMainEvents);
+    const response = yield call(API.fetchMainEvents);
     const events = response.data;
 
     yield put({ type: "API_CALL_SUCCESS_MAIN", events });
